d1eaed8314eb3b8eca7b8c74ab3df050d6fa4446
user145182 <user145182@74-80-245-18.ic2net.net>
2018-09-21 09:53:17 -0700

    Made keyboards decimal, and added tap gesture to hide keyboard

--- conversionCalc/ViewController.swift
+++ conversionCalc/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let detectTouch = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
     }
 
 
